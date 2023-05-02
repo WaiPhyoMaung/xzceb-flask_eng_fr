37c61b71888ae729053e7884ee267020de646207
WaiPhyoMaung <waiphyomaung7@gmail.com>
2023-05-02 21:54:21 +0630

    Final Changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,39 @@
+"""
+    Author: Wai Phyo Maung
+"""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+load_dotenv()
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+language_translator.set_service_url(url)
+# print(language_translator)
+def english_to_french(english_text):
+    """
+    Translate English to French
+    :param englishText:
+    :return: French text    
+    """
+    if english_text == "":
+        return ""
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    """
+    Translate French to English
+    :param frenchText:
+    :return: English text
+    """
+    if french_text == "":
+        return ""
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -3,22 +3,17 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask("Web Translator")
-
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
-
+    return json.dumps(translator.translate(textToTranslate, 'en', 'fr'))  # returns a JSON string with the
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
-
-@app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+    return json.dumps(translator.translate(textToTranslate, 'fr', 'en'))  # returns a JSON string with the      
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
